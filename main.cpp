9011bc17298f8140b3f7d53aaadc231aa0f088d5
AVKazmirchuk <85755971+AVKazmirchuk@users.noreply.github.com>
2021-10-24 21:30:07 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <array>
 #include <vector>
 #include <deque>
@@ -38,7 +38,7 @@ void checkInput(T& elem)
         std::make_signed_t<std::decay_t<T>> tmp;
 
         std::cin >> tmp;
-        
+
         if (cinNoFail())
         {
             if (std::is_unsigned_v<std::decay_t<T>> && tmp < 0)
@@ -65,88 +65,97 @@ void input(std::array<T, size>& container)
 }
 
 template <typename T>
-void input(T& container)
+void input(T* container, long long size)
 {
+    T elem{};
     
-    
-        long long size;
-
-        while (true)
-        {
-            std::cout << "Number of items in the container: ";
-            
-            std::cin >> size;
-
-            if (cinNoFail())
-            {
-                
-                break;
-            }
-            std::cout << "Invalid data!\n";
-        }
-
-
-        typename T::value_type elem{};
-
-        /*for (auto& elem : container)
-        {
-            checkInput(elem);
-            container.push_back(elem);
-        }*/
-        
-        if constexpr (std::is_same_v<T, std::forward_list<typename T::value_type>>)
-        {
-            for (int i{}; i < size; ++i)
-            {
-                checkInput(elem);
-                container.push_front(elem);
-            }
-        }
-        else
-        {
-            for (int i{}; i < size; ++i)
-            {
-                checkInput(elem);
-                container.push_back(elem);
-            }
-        }
-                
-            
-        
-    
+    for (int i{}; i < size; ++i)
+    {
+        checkInput(elem);
+        container[i] = elem;
+    }
 }
 
-
 template <typename T>
-auto arithmeticMean(const T& container, long double size = 0)
+void input(T& container)
 {
-    long double tmp{};
-    //typename T::value_type tmp{};
+    long long size;
 
-    if constexpr (!std::is_array_v<T> && !std::is_pointer_v<T>)
+    while (true)
     {
-        if constexpr (std::is_same_v<T, std::forward_list<typename T::value_type>>) size = std::distance(container.begin(), container.end());
-        else size = container.size();
+        std::cout << "Number of items in the container: ";
 
-        for (const auto& elem : container)
+        std::cin >> size;
+
+        if (cinNoFail())
         {
-            tmp += elem / size;
-            std::cout << tmp << " ";
+
+            break;
+        }
+        std::cout << "Invalid data!\n";
+    }
+
+    typename T::value_type elem{};
+
+    if constexpr (std::is_same_v<T, std::forward_list<typename T::value_type>>)
+    {
+        for (int i{}; i < size; ++i)
+        {
+            checkInput(elem);
+            container.push_front(elem);
+        }
+    }
+    else if constexpr (std::is_same_v<T, std::set<typename T::value_type>> || 
+        std::is_same_v<T, std::multiset<typename T::value_type>> || 
+        std::is_same_v<T, std::unordered_set<typename T::value_type>> || 
+        std::is_same_v<T, std::unordered_multiset<typename T::value_type>>)
+    {
+        for (int i{}; i < size; ++i)
+        {
+            checkInput(elem);
+            container.insert(elem);
         }
     }
     else
     {
-        std::cout << "array:\n";
-
-        if (std::is_array_v<T>) size = std::extent_v<T>;
-
         for (int i{}; i < size; ++i)
         {
-            tmp += container[i] / size;
-            std::cout << tmp << " ";
+            checkInput(elem);
+            container.push_back(elem);
         }
     }
+}
 
+template <typename T>
+auto arithmeticMean(const T* container, long long size)
+{
+    long double tmp{};
+
+    for (int i{}; i < size; ++i)
+    {
+        tmp += static_cast<long double>(container[i]) / size;
+        std::cout << tmp << " ";
+    }
+
+    if constexpr (std::is_same_v<T, char*>) return static_cast<char>(tmp);
+    else return tmp;
+}
+
+template <typename T>
+auto arithmeticMean(const T& container)
+{
+    long double tmp{};
+    size_t size;
+       
+    if constexpr (std::is_same_v<T, std::forward_list<typename T::value_type>>) size = std::distance(container.begin(), container.end());
+    else size = container.size();
+
+    for (const auto& elem : container)
+    {
+        tmp += static_cast<long double>(elem) / size;
+        std::cout << tmp << " ";
+    }
+       
     if constexpr (std::is_same_v<typename T::value_type, char>) return static_cast<char>(tmp);
     else return tmp;
 }
@@ -165,16 +174,38 @@ int main()
     input(d2);
     std::cout << arithmeticMean(d2) << '\n';*/
 
-    std::list<int> l2;
+    /*std::list<int> l2;
     input(l2);
-    std::cout << arithmeticMean(l2) << '\n';
+    std::cout << arithmeticMean(l2) << '\n';*/
 
-    std::forward_list<int> fl2;
+    /*std::forward_list<int> fl2;
     input(fl2);
-    std::cout << arithmeticMean(fl2) << '\n';
+    std::cout << arithmeticMean(fl2) << '\n';*/
 
+    /*std::set<int> s2;
+    input(s2);
+    std::cout << arithmeticMean(s2) << '\n';*/
 
+    /*std::multiset<int> ms2;
+    input(ms2);
+    std::cout << arithmeticMean(ms2) << '\n';*/
 
+    /*std::unordered_set<int> us2;
+    input(us2);
+    std::cout << arithmeticMean(us2) << '\n';*/
+
+    /*std::unordered_multiset<int> ums2;
+    input(ums2);
+    std::cout << arithmeticMean(ums2) << '\n';*/
+
+    /*int as2[9]{};
+    input(as2, 9);
+    std::cout << arithmeticMean(as2, 9) << '\n';*/
+
+    int* ad2 = new int[9]{};
+    input(ad2, 9);
+    std::cout << arithmeticMean(ad2, 9) << '\n';
+    delete[] ad2;
 
 
 
@@ -184,7 +215,6 @@ int main()
     std::vector<int> v{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     input(v);
     std::cout << arithmeticMean(v) << '\n';
-    
 
     std::deque<int> d{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     std::cout << arithmeticMean(d) << '\n';
@@ -208,9 +238,9 @@ int main()
     std::cout << arithmeticMean(ums) << '\n';
 
     int as[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    std::cout << arithmeticMean(as) << '\n';
+    std::cout << arithmeticMean(as, 9) << '\n';
 
-    int* ad = new int[9]{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    /*int* ad = new int[9]{ 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     std::cout << arithmeticMean(ad, 9) << '\n';
     delete[] ad;*/
 
